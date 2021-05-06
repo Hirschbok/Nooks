932ba7b7f93582bd3d1313709eace2a3e0172f0b
Hirschbok <edlapointe03@gmail.com>
2021-05-06 12:49:30 -0400

    Guhh did some updating

--- src/main/java/com/hirschbok/nooks/items/RichBoneMeal.java
+++ src/main/java/com/hirschbok/nooks/items/RichBoneMeal.java
@@ -3,11 +3,8 @@ package com.hirschbok.nooks.items;
 import com.hirschbok.nooks.Nooks;
 import net.minecraft.item.Item;
 
-
 public class RichBoneMeal extends Item {
     public RichBoneMeal() {
-        super(new Item.Properties()
-                .group(Nooks.TAB)
-                );
+        super(new Item.Properties().group(Nooks.TAB));
     }
 }
